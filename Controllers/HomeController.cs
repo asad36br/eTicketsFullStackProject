ff87574b30fa5a04fc129148c3ebe47f3f0d7b3b
Asad Tariq <asad36br@hotmail.com>
2024-02-14 08:40:20 -0500

    firstCommit for test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using eTicketsFullStackProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace eTicketsFullStackProject.Controllers
 {
@@ -16,6 +12,9 @@ namespace eTicketsFullStackProject.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+
+
         }
 
         public IActionResult Index()
